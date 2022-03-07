d05ea9fa7685dcac97bde9a4c20d95d6035d765d
Metis Teng <greatmetis@gmail.com>
2022-03-07 23:50:48 +1000

    bug:fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
-set -env
+set -e
+
 npm run build
 
 cd dist
 
-git init 
+git init
 git add -A
 git commit -m 'deploy'
+
 git push -f git@github.com:greatmetis/rest-countries-api-with-color-theme-switcher.git master:gh-pages
 
 cd-
